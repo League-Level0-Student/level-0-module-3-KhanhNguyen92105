43c25a09ecb45a37d488a27a7075eaf29f0da728
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-27 13:20:09 -0800

    Chris's mother

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -2,20 +2,83 @@
 //    Level 0
 
 package _01_robot_color_chooser;
-
+import org.jointheleague.graphical.robot.Robot;
 import javax.swing.JOptionPane;
+import java.util.Random;
 import java.awt.Color;
-
+import org.jointheleague.graphical.robot.Robot;
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		String Color = JOptionPane.showInputDialog("what color do you want, red, green, or blue?");
+		Robot  e = new Robot();
+		e.setPenWidth(10);
+		e.penDown();
+		e.setSpeed(140);
+		Random ran = new Random();
+		int re = 0;
+		int re1 = 0;
+		int re2 = 0;
+		re = ran.nextInt(44);
+		re1 = ran.nextInt(255);
+		re2 = ran.nextInt(255);
 		//3. Ask the user what color they would like the robot to draw
 		
 		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
+		if(Color.equalsIgnoreCase("Blue")) {
+			
+			e.setPenColor(0, 0, 255);
+			e.move(250);
+			e.turn(90);
+			e.move(250);
+			e.turn(90);
+			e.move(250);
+			e.turn(90);
+			e.move(250);
+			}
+			if(Color.equalsIgnoreCase("Red")) {
+				e.setPenColor(255,0,0);
+				e.move(250);
+				e.turn(90);
+				e.move(250);
+				e.turn(90);
+				e.move(250);
+				e.turn(90);
+				e.move(250);
+				
+			}
+			     if(Color.equalsIgnoreCase("Green")) {
+			    	 e.setPenColor(0, 255, 0);
+			    	 e.move(250);
+			    		e.turn(90);
+			    		e.move(250);
+			    		e.turn(90);
+			    		e.move(250);
+			    		e.turn(90);
+						e.move(250);
+			     }
+			     
+			     else {
+			    	 if(Color.equalsIgnoreCase("Green")) {
+			    		 e.penUp();
+			    	 }
+			    	 if(Color.equalsIgnoreCase("Red")) {
+			    		 e.penUp();
+			    	 }
+			    	 if(Color.equalsIgnoreCase("Blue")) {
+			    		 e.penUp();
+			    	 }
+			    	 e.setPenColor(re, re1, re2);
+			    	
+						e.move(250);
+						e.turn(90);
+						e.move(250);
+						e.turn(90);
+						e.move(250);
+						e.turn(90);
+						e.move(250);
+			     }
+   //6. If the user doesn’t enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -5,21 +5,27 @@ package _03_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100-1);
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
 
+String number = JOptionPane.showInputDialog("Guess a number from 1 to 100");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int number1 = Integer.parseInt(number);
 			// 5. if the guess is correct
+			if(number1 == random) {
+				JOptionPane.showMessageDialog(null, "You've won!!!");
+			}
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
